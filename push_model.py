bc0c12ca55c970c57e72257d36cbca036248576f
LifuWang-66 <79244724+LifuWang-66@users.noreply.github.com>
2025-04-12 03:47:05 +0800

    Delete push_model.py

--- push_model.py
+++
@@ -1,7 +0,0 @@
-import torch
-from models.T5_encoder import T5EncoderWithProjection, T5ProjectionConfig
-state_dict = torch.load("ckpt/flux_flan_t5_base_512_combined_continue_iter180000.pth")
-config = T5ProjectionConfig.from_pretrained('ckpt/config.json')
-model = T5EncoderWithProjection(config)
-model.load_state_dict(state_dict)
-model.push_to_hub("ScalingDownTextEncoder")
